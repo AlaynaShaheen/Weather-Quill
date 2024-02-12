ab5fa5700cc954d517fb55f2a9fe7e9ad6db4d08
Alayna Shaheen <alaynashaheen2@gmail.com>
2024-02-12 23:54:38 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
+import { createHashRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import WeatherApp from './Components/WeatherApp/WeatherApp';
 import Initial from './Components/Initial/Initial';
-const router=createBrowserRouter(
+const router=createHashRouter(
     createRoutesFromElements(<>
       <Route path='/' element={<App/>}/>
          <Route path='/weatherapp' element={<WeatherApp/>}/>
